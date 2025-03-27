f1458b07cf3008b3724612402c51dc23359b6366
codingbrowny <codingbrowny@gmail.com>
2025-03-27 18:36:51 +0000

    basic data types

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// INTEGERS - INT8, INT16 INT32 INT64
+	var age int = 20
+	fmt.Println(age)
+
+
+	//STRINGS
+	var name string = "Browny"
+	fmt.Println(name)
+
+	//BOOLEAN
+	var isActive bool = false
+	var isOpened bool = true
+	fmt.Println(isActive, isOpened)
+
+	//FLOATS - FLOAT32, FLOAT64
+	var price float32 = 5.99
+	fmt.Println(price)
+}
